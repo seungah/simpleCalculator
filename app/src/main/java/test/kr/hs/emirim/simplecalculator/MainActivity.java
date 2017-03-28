b1914b339f0ba3b15b17f35a59acb2eff9a810cb
HyeonJu Jeong <0106grace@naver.com>
2017-03-28 20:15:39 +0900

    #2 Event Programming

--- app/src/main/java/test/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/test/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
     EditText edit_first, edit_second;
     TextView text_Result;
 
@@ -24,15 +24,73 @@ public class MainActivity extends AppCompatActivity {
         Button but_multiply=(Button)findViewById(R.id.but_multiply);
         Button but_division=(Button)findViewById(R.id.but_division);
         text_Result=(TextView)findViewById(R.id.text_result);
+        but_plus.setOnClickListener(butHandler);
+        but_minus.setOnClickListener(butHandler);
+        but_multiply.setOnClickListener(butHandler);
+        but_division.setOnClickListener(butHandler); //setOnClickListener를 붙이지 않으면 버튼을 눌러도 안눌림.
 
-        but_plus.setOnClickListener(new View.OnClickListener() {
+
+
+        /*but_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int num1=Integer.parseInt(edit_first.getText().toString());
                 int num2=Integer.parseInt(edit_second.getText().toString());
                 text_Result.setText(num1 + num2+""); //문자열이 아니기 때문에 실행하면 오류가 남.
             }
-        });
+        });*/
 
-    }
+    }//end oncreat
+
+    View.OnClickListener butHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int num1=Integer.parseInt(edit_first.getText().toString());
+            int num2=Integer.parseInt(edit_second.getText().toString());
+            int result = 0;
+
+            switch (v.getId()){
+                case R.id.but_plus:
+                    result=num1+num2;
+                    break;
+                case R.id.but_minus:
+                    result=num1-num2;
+                    break;
+                case R.id.but_multiply:
+                    result=num1*num2;
+                    break;
+                case R.id.but_division:
+                    result=num1/num2;
+                    break;
+            }
+
+            text_Result.setText(num1 + num2+"");
+
+        }
+    };
+
+    /*@Override
+    public void onClick(View v) {
+        int num1=Integer.parseInt(edit_first.getText().toString());
+        int num2=Integer.parseInt(edit_second.getText().toString());
+        int result = 0;
+
+        switch (v.getId()){
+            case R.id.but_plus:
+                result=num1+num2;
+                break;
+            case R.id.but_minus:
+                result=num1-num2;
+                break;
+            case R.id.but_multiply:
+                result=num1*num2;
+                break;
+            case R.id.but_division:
+                result=num1/num2;
+                break;
+        }
+
+        text_Result.setText(num1 + num2+"");
+
+    }*/
 }
